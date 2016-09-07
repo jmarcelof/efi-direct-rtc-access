8d7f4f4d4fe0d4075899755bcde1ab15a9f50a62
Marcelo Ferreira <joaomarcelo@lesc.ufc.br>
2016-09-07 16:24:59 -0300

    Add locate pci root bridge io protocol
    Add locate protocol asking for EFI_PCI_ROOT_BRIDGE_IO_PROTOCOL.

--- main.c
+++ main.c
@@ -3,16 +3,24 @@
 
 #define SEC_TO_USEC(value) ((value) * 1000 * 1000)
 
+EFI_GUID gEfiPciRootBridgeIoProtocolGuid = { 0x2f707ebb, 0x4a1a, 0x11d4, {0x9a, 0x38, 0x00, 0x90, 0x27, 0x3f, 0xc1, 0x4d } };
+
 EFI_STATUS
 EFIAPI
 efi_main (
     EFI_HANDLE          ImageHandle,
     EFI_SYSTEM_TABLE    *SystemTable)
 {
+    EFI_STATUS 		Status = EFI_SUCCESS;
+    VOID 		*PciRootBridge = NULL;
 
     InitializeLib (ImageHandle, SystemTable);
 
     Print (L"Hello, world!\n");
+
+    Status = uefi_call_wrapper (BS->LocateProtocol, 3, &gEfiPciRootBridgeIoProtocolGuid, NULL, (VOID **) &PciRootBridge);
+    Print (L"Status = %r\n", Status);
+
     uefi_call_wrapper (BS->Stall, 1, SEC_TO_USEC (5));
 
     return (EFI_SUCCESS);
