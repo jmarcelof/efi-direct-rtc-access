2ca78194f98685e6b88373d6fd39e3ac2ff9bd74
Marcelo Ferreira <joaomarcelo@lesc.ufc.br>
2016-09-09 15:58:34 -0300

    Convert read time to good format
    Consider only hh:mm:ss and convert both from BCD and from 12h format when applied.

--- main.c
+++ main.c
@@ -3,34 +3,38 @@
 
 #include "PciRootBridgeIo.h"
 
-#define SEC_TO_USEC(value) ((value) * 1000 * 1000)
+#define SEC_TO_USEC(Value) ((Value) * 1000 * 1000)
 
 EFI_GUID gEfiPciRootBridgeIoProtocolGuid = EFI_PCI_ROOT_BRIDGE_IO_PROTOCOL_GUID;
 EFI_PCI_ROOT_BRIDGE_IO_PROTOCOL *PciRootBridge = NULL;
 
 VOID
 PrintTime (
-  EFI_TIME            *time);
+    EFI_TIME            *Time);
 
 VOID
 ReadTime (
-  EFI_TIME            *time);
+    EFI_TIME            *Time);
+
+UINT16
+BcdToDecimal (
+    UINT16              Value);
 
 VOID
 WaitStopUpdate ();
 
 UINT8
 ReadRTCPort (
-  UINT64              port);
+    UINT64              Port);
 
 VOID
 WritePort8 (
-  UINT64              port,
-  UINT8               value);
+    UINT64              Port,
+    UINT8               Value);
 
 UINT8
 ReadPort8 (
-  UINT64              port);
+    UINT64              Port);
 
 EFI_STATUS
 EFIAPI
@@ -38,18 +42,18 @@ efi_main (
     EFI_HANDLE          ImageHandle,
     EFI_SYSTEM_TABLE    *SystemTable)
 {
-    EFI_STATUS 		       Status = EFI_SUCCESS;
-    EFI_TIME             time;
+    EFI_STATUS 		Status = EFI_SUCCESS;
+    EFI_TIME            Time;
 
     InitializeLib (ImageHandle, SystemTable);
 
     Print (L"Hello, world!\n");
 
-    Status = uefi_call_wrapper (BS->LocateProtocol, 3, &gEfiPciRootBridgeIoProtocolGuid, NULL, (VOID **) &PciRootBridge);
+    Status = uefi_call_wrapper (BS->LocateProtocol, 3, &gEfiPciRootBridgeIoProtocolGuid, NULL, (VOID **) & PciRootBridge);
     Print (L"Status = %r\n", Status);
 
-    ReadTime (&time);
-    PrintTime (&time);
+    ReadTime (&Time);
+    PrintTime (&Time);
 
     uefi_call_wrapper (BS->Stall, 1, SEC_TO_USEC (5));
 
@@ -58,26 +62,42 @@ efi_main (
 
 VOID
 PrintTime (
-  EFI_TIME            *time)
+    EFI_TIME            *Time)
 {
 
-    Print (L"%2uh%02umin%02useg - %02u/%02u/%04u\n",
-      time->Hour, time->Minute, time->Second,
-      time->Month, time->Day, time->Year);
+    Print (L"%2hhd:%02hhd:%02hhd\n",
+        Time->Hour, Time->Minute, Time->Second);
 }
 
 VOID
 ReadTime (
-  EFI_TIME            *time)
+    EFI_TIME            *Time)
 {
 
     WaitStopUpdate ();
-    time->Second = ReadRTCPort(0x00);
-    time->Minute = ReadRTCPort(0x02);
-    time->Hour = ReadRTCPort(0x04);
-    time->Day = ReadRTCPort(0x07);
-    time->Month = ReadRTCPort(0x08);
-    time->Year = ReadRTCPort(0x09);
+    Time->Second = ReadRTCPort (0x00);
+    Time->Minute = ReadRTCPort (0x02);
+    Time->Hour = ReadRTCPort (0x04);
+    // is BCD coded, then convert to decimal
+    if ((ReadRTCPort (0x0B) & 0x04) == 0x00)
+    {
+        Time->Second = BcdToDecimal (Time->Second);
+        Time->Minute = BcdToDecimal (Time->Minute);
+        Time->Hour = BcdToDecimal (Time->Hour);
+    }
+    // is 12h format, then convert to 24h format
+    if (((ReadRTCPort (0x0B) & 0x02) == 0x00) && (Time->Hour & 0x80))
+    {
+        Time->Hour = ((Time->Hour & 0x7F) + 24) % 24;
+    }
+}
+
+UINT16
+BcdToDecimal (
+    UINT16              Value)
+{
+
+    return ((Value & 0x0F) + ((Value >> 4) * 10));
 }
 
 VOID
@@ -93,11 +113,11 @@ WaitStopUpdate ()
 
 UINT8
 ReadRTCPort (
-    UINT64              port)
+    UINT64              Port)
 {
     UINT8               Value;
 
-    WritePort8 (0x70, port);
+    WritePort8 (0x70, Port);
     Value = ReadPort8 (0x71);
 
     return (Value);
@@ -105,24 +125,22 @@ ReadRTCPort (
 
 VOID
 WritePort8 (
-    UINT64              port,
-    UINT8               value)
+    UINT64              Port,
+    UINT8               Value)
 {
-    EFI_STATUS      Status = EFI_SUCCESS;
+    EFI_STATUS          Status = EFI_SUCCESS;
 
-    Status = uefi_call_wrapper (PciRootBridge->Io.Write, 5, PciRootBridge, EfiPciWidthUint8, port, 1, &value);
-    Print (L"WritePort8 Status = %r\n", Status);
+    Status = uefi_call_wrapper (PciRootBridge->Io.Write, 5, PciRootBridge, EfiPciWidthUint8, Port, 1, &Value);
 }
 
 UINT8
 ReadPort8 (
-    UINT64              port)
+    UINT64              Port)
 {
     UINT8               Value;
-    EFI_STATUS      Status = EFI_SUCCESS;
+    EFI_STATUS          Status = EFI_SUCCESS;
 
-    Status = uefi_call_wrapper (PciRootBridge->Io.Read, 5, PciRootBridge, EfiPciWidthUint8, port, 1, &Value);
-    Print (L"ReadPort8 Status = %r\n", Status);
+    Status = uefi_call_wrapper (PciRootBridge->Io.Read, 5, PciRootBridge, EfiPciWidthUint8, Port, 1, &Value);
 
     return (Value);
 }
